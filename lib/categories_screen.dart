3e1e6f1b148fdd9069def3fe5a70c869e9cf2e5b
Disha Arora <darora1996@fincodetech.in>
2022-02-22 18:54:17 +0530

    Second commit

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -14,13 +14,14 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text('DeliMeal'),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(25),
         gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
           childAspectRatio: 3/2,
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
         ),
-        children: DUMMY_CATEGORIES.map((catData) => CategoryItem(catData.title, catData.color),).toList()
+        children: DUMMY_CATEGORIES.map((catData) => CategoryItem(catData.id, catData.title, catData.color),).toList()
         ,
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/categories_screen.dart';
+import 'package:meals_app/category_meals_screen.dart';
+import 'package:meals_app/models/category.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,10 +16,27 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Deli Meals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          secondary: Colors.amber,
+        ),
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+        textTheme: ThemeData.light().textTheme.copyWith(
+          bodyText1: const TextStyle(
+            color: Color.fromRGBO(20, 51, 51, 1)
+          ),
+          bodyText2: const TextStyle(
+              color: Color.fromRGBO(20, 51, 51, 1)
+          ),
+          subtitle1: const TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
+
+        )
+
       ),
-      home:  const CategoriesScreen(),
+      home: const CategoriesScreen(),
+      routes: {
+        '/category-meals':(ctx) => CategoryMealsScreen(),
+      },
     );
   }
 }
-
